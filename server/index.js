4ee816823b0bfd9beb5630d8270db60d400d0637
Krystian Cichorz <77442325+Krrystian@users.noreply.github.com>
2023-12-14 20:22:37 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: "true" }));
 
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: "https://mern-todo-frontend-nine.vercel.app",
   credentials: true,
 };
 app.use(cors(corsOptions));
