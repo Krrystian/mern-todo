de5561722b7d9280f40327e89db8ed6808536271
Krystian Cichorz <krystiancichorz708@gmail.com>
2023-12-16 14:43:33 +0100

    Fix register

--- server/routes/user.js
+++ server/routes/user.js
@@ -7,7 +7,7 @@ import {
 } from "../controllers/user.js";
 import { verifyToken } from "../middleware/auth.js";
 const router = express.Router();
-router.post("/register", verifyToken, register);
+router.post("/register", register);
 router.post("/newPassword", verifyToken, newPassword);
 router.post("/newUsername", verifyToken, newUsername);
 router.post("/newEmail", verifyToken, newEmail);
